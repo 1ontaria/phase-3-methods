d35f0f827bd3013af2b657ff5f789cdc30b7d3a3
Ontaria Woods <ontaria.woods@gmail.com>
2023-06-20 19:50:31 -0400

    commit tree 20660503c91aa08d537ace0ff64218a498ea2ff5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,59 @@
-# Your code here!
+# function greetProgrammer() {
+#     console.log("Hello, programmer!");
+#   }
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+
+greet("Ontaria")
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add num1, num2
+    num1 + num2
+end
+
+sum = add(1, 2)
+puts sum
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+
+def halve num
+if num.class != Integer
+     return nil
+end
+
+num / 2 
+end
+
+result = halve(4)
+puts result
